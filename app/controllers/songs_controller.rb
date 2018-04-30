e8195f365f01c92becc44433aa16cee0424d0c61
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-04-30 03:34:21 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,9 +24,7 @@ class SongsController < ApplicationController
   end
 
   def upload
-    CSV.foreach(params["file"].path, headers: true) do |song|
-      Song.create(title: song[0], artist_name: song[1])
-    end
+    SongsWorker.perform_async(params[:file].path)
     redirect_to songs_path
   end
 
@@ -58,5 +56,5 @@ class SongsController < ApplicationController
   def song_params
     params.require(:song).permit(:title, :artist_name)
   end
-end
 
+end
